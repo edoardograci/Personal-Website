8a9dbc5995d43d25db7124454d271987f5de23e8
Edoardo Graci <edoardograci@gmail.com>
2025-05-04 22:20:07 +0200

    Simple Analytics added options

--- WEBSITE/netlify/functions/framer-proxy.ts
+++ WEBSITE/netlify/functions/framer-proxy.ts
@@ -80,7 +80,8 @@ export const handler = async (event) => {
           });
         </script>
         <!-- 100% privacy-first analytics -->
-        <script async src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
+        <script data-collect-dnt="true" async src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
+        <noscript><img src="https://queue.simpleanalyticscdn.com/noscript.gif?collect-dnt=true" alt="" referrerpolicy="no-referrer-when-downgrade"/></noscript>
       </body>`);
 
       return {
